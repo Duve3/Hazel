805d1dd788bda1fd1a17a69dbb0217a32fd1b8c4
Duve3 <Duv3tabest@gmail.com>
2024-04-07 20:20:07 -0700

    Logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!"); 
+	int a = 5;
+	HZ_INFO("Hello! Var={0} Var+1={1}", a, a+1);
+
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
